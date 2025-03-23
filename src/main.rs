835836e69a8249492e0c9038b3597bdaf5b19ef8
ItsElijahWood <itselijahwood@gmail.com>
2025-03-23 15:04:32 +0000

    feat: removed auto startup

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,9 @@
-use std::process::Command;
-
 mod server;
 mod modules;
 
 const ADDR: &str = ":3000";
 fn main() {
     modules::read_centre_file::read_centre_file();
-
-    let url = "http://127.0.0.1:3000";
-
-    #[cfg(target_os = "windows")]
-    if let Err(e) = Command::new("start http://127.0.0.1:3000") 
-        .arg(url)
-        .spawn()
-    {
-        eprintln!("Error executing default web browser command: {}", e);
-    }
-
-    #[cfg(target_os = "linux")]
-    if let Err(e) = Command::new("xdg-open") 
-        .arg(url)
-        .spawn()
-    {
-        eprintln!("Error executing default web browser command: {}", e);
-    }
 
     server::server(ADDR);
 }
